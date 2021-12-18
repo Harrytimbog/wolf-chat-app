91888dd840acc93582be850fb31661addce150fd
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-18 21:28:31 +0100

    .

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -51,7 +51,7 @@ const ChatFeed = (props) => {
       <div className="chat-title-container">
         <div className="chat-title">{chat.title}</div>
         <div className="chat-subtitle">
-          {chat.people.map((person) => `${person.person.username}` )}
+          {chat.people.map((person) => ` ${person.person.username}` )}
         </div>
       </div>
       {renderMessages()}
